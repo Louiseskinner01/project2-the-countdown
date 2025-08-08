69a932ac17994422f65a464c9b6d08bd6ee2e2eb
Louise sKinner <louise.skinner40@gmail.com>
2025-08-08 10:31:02 +0100

    Add JS functionality to HTMl PLAY text

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+let confirmDifficultyBtn;
+let difficultyBtns;
+let timerDisplay;
+let playSpan;
+let controllersArea = document.getElementById("controllers-area");
+const gamesConsole = document.getElementById("games-console");
+
+//Display buttons to set difficulty level and hide the "PLAY" text
+function displayControllers() {
+    document.getElementById("difficulty-btn-area").style.visibility = "visible";
+    document.getElementById("timer-area").style.visibility = "visible";
+    document.getElementById("console-play").style.visibility = "hidden";
+    gamesConsole.innerText = "Select a difficulty level, this will update the timer. Once you have decided what level to play at, click the confirm level button below. ";
+    
+}
