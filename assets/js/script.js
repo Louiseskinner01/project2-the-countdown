d7674cdf9b63e6152a574530ab23ebbec9d2cec9
Louise sKinner <louise.skinner40@gmail.com>
2025-08-08 21:36:23 +0100

    Add functions to enable the user to select a difficulty level, update the timer and confirm their chosen level

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,33 @@ function createConfirmBtn() {
     controllersArea.append(confirmDifficultyBtn);
     
 }
+
+/**
+ * This two functions below allow the user to select a difficulty level.
+ * The first function (updateTimerDisplay(seconds)) takes seconds as it value and siaplys the value in 00:00 format
+ * The second function updates the timer and confirm the level.
+ */
+
+let countdownInterval;
+let startTime;
+let selectedDifficulty = 00; // Timer digits are set to 00:00 as default
+
+// Update timer display 
+function updateTimerDisplay(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    document.getElementById("timer-digits").textContent =
+        `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
+}
+
+// Set the difficulty level and update timer display
+function setDifficulty(seconds) {
+    selectedDifficulty = seconds;
+    updateTimerDisplay(seconds);
+    function appendConfirmButton(){
+        createConfirmBtn();
+    }
+    appendConfirmButton();
+}
+//This line assigns the setDifficulty function as a property on the global window object so it can be accessed anywhere
+window.setDifficulty = setDifficulty;
