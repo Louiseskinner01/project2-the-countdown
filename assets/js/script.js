5e8633eb1b5d6f9e3dbbe78682bdf84ca7823a94
Louise sKinner <louise.skinner40@gmail.com>
2025-08-12 21:06:43 +0100

    Remove orientation function from script .js and ADD it to the orientation.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,3 @@
-function checkOrientation() {
-    const orientationMessage = document.getElementById("rotate-warning");
-    const isMobile = window.innerWidth < 576;
-    const isPortrait = window.innerHeight > window.innerWidth;
-    
-    if (orientationMessage) {
-        if (isMobile && isPortrait) {
-            orientationMessage.style.display = "flex";
-            document.getElementById("console-play").style.display = "none";
-        } else {
-            orientationMessage.style.display = "none";
-            document.getElementById("console-play").style.display = "block"
-        }
-    }
-    }
-    
-    // Run on load and when orientation or size changes
-    window.addEventListener('load', checkOrientation);
-    window.addEventListener('resize', checkOrientation);
-    window.addEventListener('orientationchange', checkOrientation);
-
-
-
-
 let confirmDifficultyBtn;
 let difficultyBtns;
 let timerDisplay;
