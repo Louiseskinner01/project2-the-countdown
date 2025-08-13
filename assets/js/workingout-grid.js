fb8151c3ff684d1d3a7d6d9a8b01d65cc2e45c5d
Louise sKinner <louise.skinner40@gmail.com>
2025-08-13 14:47:39 +0100

    Create function names (no functionality just names)

---
+++ assets/js/workingout-grid.js
@@ -0,0 +1,8 @@
+function createEquationRow() { /* creates and appends a new row */ }
+
+function handleInputEnter(event) { /* listens for Enter key */ }
+
+function evaluateExpression(inputValue) { /* returns result */ }
+
+
+
